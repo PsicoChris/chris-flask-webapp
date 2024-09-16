8060b1aba8341147a0be078e627d21b5f98ef9c1
Christian David Lancheros Martínez <109816345+PsicoChris@users.noreply.github.com>
2024-09-16 18:56:21 +0000

    Puse el proyecto de flask

---
+++ app.py
@@ -0,0 +1,13 @@
+#esto es para iniciar la aplicación de Flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "Hello, sapoperro"
+
+#esto es para que corra el flask sin dañar las otras configs
+
+if __name__ == "__main__":
+  app.run(host= '0.0.0.0', debug=True)
