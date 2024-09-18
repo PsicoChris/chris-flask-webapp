4cc310d7860f8703bde8cac088491675ea11be86
Christian David Lancheros Martínez <109816345+PsicoChris@users.noreply.github.com>
2024-09-18 15:56:17 +0000

    Se le puso dynamic data y ruta de API

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 #esto es para iniciar la aplicación de Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+#esto es la lista de trabajos disponibles
+JOBS = [
+  {
+    'id' : 1,   
+    'title' : 'Data Analyst',
+    'location' : 'Bogota, Colombia',
+    'salary' : 'COP $12,000,000',
+  },
+  {
+    'id' : 2,   
+    'title' : 'Data Scientist',
+    'location' : 'Bogota, Colombia',
+    'salary' : 'COP $13,000,000',
+  },
+  {
+    'id' : 3,   
+    'title' : 'Data Engineer',
+    'location' : 'Bogota, Colombia',
+    'salary' : 'COP $12,000,000',
+  },
+  {
+    'id' : 4,   
+    'title' : 'Data Analyst',
+    'location' : 'Bogota, Colombia',
+
+  }
+  
+]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="Chris.Org")
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 #esto es para que corra el flask sin dañar las otras configs
 
